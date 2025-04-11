6a5370f67df8b12f7a1cf69b38c3a825430756c8
Mani1890-coder <bodakuntlamani@gmail.com>
2025-04-11 16:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Bot is running!"
 
-@app.route("/")
-def hello_world():
-    return "Jisshu bots"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
